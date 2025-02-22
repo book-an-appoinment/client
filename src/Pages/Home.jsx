f09d8c6f7f6cd1ed00987c89cb8faa983e290eb7
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-02-22 17:16:41 +0600

    header partially completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,16 @@
+import Header from "../components/Header/Header";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header
+        heading={"Driving Growth for Your Business"}
+        para={
+          "Expert consulting drives corporate growth with creative strategy and solutions."
+        }
+      />
+    </div>
+  );
 };
 
 export default Home;
