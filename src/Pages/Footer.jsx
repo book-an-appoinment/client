04ae8ca355a0c7bd7dc69df4aa36f5c8a903b309
Softvence <softvence@Softvences-Mac-mini.local>
2025-02-22 18:09:02 +0600

    footer done

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -4,6 +4,7 @@ import { footerNavData } from "../data/footerData";
 import { FaFacebook } from "react-icons/fa6";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { FaMessage } from "react-icons/fa6";
 
 const icons = [
   {
@@ -21,64 +22,90 @@ const Footer = () => {
   return (
     <div className="bg-footer-background">
       <CommonWrapper>
-        {/* ===footer top=== */}
-        <div className="flex">
-          <div className="w-1/2 flex flex-col gap-10">
-            <h3 className="text-white text-[32px] font-railway font-bold">
-              Financy
-            </h3>
-            <p className="w-[430px] text-[18px] text-white opacity-50">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa
-              distinctio quibusdam, reprehenderit
-            </p>
-          </div>
-          <div className="w-1/2 flex justify-between">
-            {footerNavData.map((item) => {
-              return (
-                <div>
-                  <p className="text-xl font-semibold text-white font-railway mb-3.5">
-                    {item.header}
-                  </p>
+        <div className="py-[100px]">
+          {/* ===footer top=== */}
+          <div className="flex">
+            <div className="w-1/2 flex flex-col gap-5">
+              <h3 className="text-white text-[32px] font-railway font-bold">
+                Financy
+              </h3>
+              <p className="w-[430px] text-[18px] text-white opacity-50">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa
+                distinctio quibusdam, reprehenderit
+              </p>
+            </div>
+            <div className="w-1/2 flex justify-between">
+              {footerNavData.map((item) => {
+                return (
                   <div>
-                    {item.links.map((navItem) => {
-                      return (
-                        <Link
-                          to={navItem.url}
-                          className="text-white opacity-50 text-[18px] block font-normal"
-                        >
-                          {navItem.Label}
-                        </Link>
-                      );
-                    })}
+                    <p className="text-xl font-semibold text-white font-railway mb-3.5">
+                      {item.header}
+                    </p>
+                    <div>
+                      {item.links.map((navItem) => {
+                        return (
+                          <Link
+                            to={navItem.url}
+                            className="text-white opacity-50 text-[18px] block font-normal"
+                          >
+                            {navItem.Label}
+                          </Link>
+                        );
+                      })}
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
-        </div>
-        {/* ===divider=== */}
-        <div className="border-t border-white border-[1.5px] my-4"></div>
+          {/* ===divider=== */}
+          <div className="border-t border-white border-[1.5px] my-11"></div>
 
-        {/* ===icon=== */}
-        <div className="flex justify-between">
-          <p className="text-[16px] text-white">
-            Copyright 2024 <span className="font-bold">Financy</span> reserved
-          </p>
-          <div className="flex gap-4 py-5">
-            {icons.map((item) => {
-              const SelectedIcon = item.icon;
-              return (
-                <p className="border-[1.5px] border-white rounded-full flex items-center justify-center p-2">
-                  <SelectedIcon className="text-white" />
+          {/* ===icon=== */}
+          <div className="flex justify-between items-center">
+            <p className="text-[16px] text-white">
+              Copyright 2024 <span className="font-bold">Financy</span> reserved
+            </p>
+            <div className="flex gap-4 py-5">
+              {icons.map((item) => {
+                const SelectedIcon = item.icon;
+                return (
+                  <p className="border-[1.5px] border-white rounded-full flex items-center justify-center p-2">
+                    <SelectedIcon className="text-white" />
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+
+          {/* ===newsLetter=== */}
+          <div className="w-full bg-light-green p-[60px] rounded-[20px] flex justify-between items-center">
+            <div className="flex gap-2">
+              <p className="bg-black rounded-full flex items-center justify-center p-6">
+                <FaMessage className="text-white text-xl" />
+              </p>
+              <div>
+                <h5 className="text-[28px] font-semibold text-[#004D3F]">
+                  Join our newsletter now
+                </h5>
+                <p className="text-[18px] text-[#004D3FCC] opacity-80">
+                  Be part of 500k community thriving with ZaiHR
                 </p>
-              );
-            })}
+              </div>
+            </div>
+            <div>
+              <div className="relative w-full max-w-md">
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  className="w-full p-3 pr-16 bg-white rounded-full focus:outline-none"
+                />
+                <button className="absolute top-1/2 right-2 -translate-y-1/2 bg-[#004D3F] text-white px-4 py-2 rounded-full text-sm">
+                  Subscribe
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-
-        {/* ===newsLetter=== */}
-        <div className="w-full bg-light-green">
-          <h1>sfdsfdsf</h1>
         </div>
       </CommonWrapper>
     </div>
