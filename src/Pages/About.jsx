28a0f8c5e2d6763f0edc09adfbfcc1ec1d294482
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-02-23 11:54:43 +0600

    Meet with our team

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,5 @@
 import CompanyValues from "../components/CommonWrapper/About/CompanyValues";
+import MeetOutTeam from "../components/CommonWrapper/About/MeetOutTeam";
 import ReliableConsultant from "../components/CommonWrapper/About/ReliableConsultant";
 import StatsCards from "../components/CommonWrapper/About/StatsCards";
 import SuccessStory from "../components/CommonWrapper/About/SuccessStory";
@@ -18,6 +19,9 @@ const About = () => {
       <Timeline />
       <CommonWrapper>
         <CompanyValues />
+      </CommonWrapper>
+      <CommonWrapper>
+        <MeetOutTeam />
       </CommonWrapper>
 
 
