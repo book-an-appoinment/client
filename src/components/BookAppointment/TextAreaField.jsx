15924c35a5d6472ea281d2672ee89a1d57aeb9f6
Softvence <softvence@Softvences-Mac-mini.local>
2025-02-24 17:19:56 +0600

    updated code

--- src/components/BookAppointment/TextAreaField.jsx
+++ src/components/BookAppointment/TextAreaField.jsx
@@ -10,14 +10,14 @@ const TextAreaField = ({
   return (
     <div className="w-full">
       {label && (
-        <label className="text-[#010205] font-semibold text-[24px] font-raleway mb-5 capitalize">
+        <label className="text-[#010205] font-semibold lg:text-[24px] font-raleway mb-5 capitalize">
           {label}
         </label>
       )}
       <textarea
         {...register(name, rules)}
         placeholder={placeholder}
-        className={`w-full px-4 py-2 border-[1px] h-[205px] border-[#004D3F40] text-[22px] rounded-md focus:outline-none focus:ring-1 focus:ring-[#14322c40] resize-none ${className}`}
+        className={`w-full px-4 py-2 border-[1px] lg:h-[205px] border-[#004D3F40] lg:text-[22px] rounded-md focus:outline-none focus:ring-1 focus:ring-[#14322c40] resize-none ${className}`}
         rows={4} // Default row size, can be overridden via className
       ></textarea>
       {errors?.[name] && (
