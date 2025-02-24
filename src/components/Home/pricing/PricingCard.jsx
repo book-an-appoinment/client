dd0f805a2e0d96bd3bc2c99c253b10be070180b3
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-02-24 16:26:55 +0600

    pricing fixed

--- src/components/Home/pricing/PricingCard.jsx
+++ src/components/Home/pricing/PricingCard.jsx
@@ -1,4 +1,3 @@
-
 /* eslint-disable react/prop-types */
 import React from "react";
 
@@ -9,8 +8,7 @@ import { MdOutlineTaskAlt } from "react-icons/md";
 const PricingCard = ({ bgColor, price, planType }) => {
   return (
     <div
-
-      className={`w-[420px] h-[557px] ${bgColor} rounded-3xl flex flex-col p-6 shadow-lg absolute-top relative`}
+      className={`w-[420px] h-[557px] mt-4 ${bgColor} rounded-3xl flex flex-col p-6 shadow-lg absolute-top relative`}
     >
       {/* Toggle Button Placeholder */}
       <div className="absolute top-[-30px] left-1/2 transform -translate-x-1/2 z-10">
@@ -23,13 +21,10 @@ const PricingCard = ({ bgColor, price, planType }) => {
       {/* Per Month/Year Section */}
       <p className="text-lg text-[#004D3F] font-urbanist">{planType}</p>
 
-
       {/* Price Section */}
       <div className="flex items-center gap-2">
         <p className="font-raleway text-[47.39px] font-bold text-[#004D3F]">
-
           ${price}
-
         </p>
         <span className="bg-[#004D3F] text-[#A7EB94] text-xs font-urbanist px-2 py-1 rounded-md">
           Save 20%
