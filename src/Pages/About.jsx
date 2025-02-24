78aee1d72fba105976ea5f8c155794252d53dc7c
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-02-24 18:15:47 +0600

    updat timline an dother

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,3 @@
-import Articles from "../components/About/Articles";
 import CompanyValues from "../components/About/CompanyValues";
 import GuidingValues from "../components/About/GuidingValues";
 import MeetOutTeam from "../components/About/MeetOutTeam";
@@ -30,7 +29,7 @@ const About = () => {
       <CommonWrapper>
         <GuidingValues />
       </CommonWrapper>
-      <Articles />
+
 
 
     </div>
