7f67ad88078792f7cd934b4c98d6f2ec58a4d570
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-02-24 18:01:25 +0600

    onboard, pioneer, article updated

--- src/components/Home/GetProfessional/OnboardPlaning.jsx
+++ src/components/Home/GetProfessional/OnboardPlaning.jsx
@@ -1,62 +1,67 @@
+import CommonWrapper from "../../CommonWrapper/CommonWrapper";
+
 const OnboardPlaning = () => {
   return (
     <div className=" mb-40">
-      <div className="ml-20 lg:mt-[650px] w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 ">
+      <CommonWrapper>
         <svg
-          className="absolute left-0 w-full"
-          height="80"
-          viewBox="0 0 1921 80"
-          fill="none"
+          className="hidden lg:block lg:absolute left-0 top-[1376px] w-full -z-10"
+          height="250"
+          viewBox="0 0 1821 80"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M-1 1 C800 200, 1120 200, 1921 1"
-            stroke="black"
-            strokeWidth="1"
+            d="M-1 20 C900 100, 1621 50, 1851 30"
+            stroke="gray"
+            strokeWidth="0.5"
             fill="transparent"
           />
         </svg>
-        <div className="w-full h-[320px] flex flex-col items-center justify-center p-6 text-center transition-transform transform hover:scale-105">
-          <div className="w-[50px] h-[50px] rounded-full bg-[#004D3F] flex items-center justify-center text-white text-3xl font-semibold relative">
-            1
+        <div className="lg:mt-[650px] w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
+          <div className="w-full flex flex-col items-center justify-center p-6 text-center transition-transform transform hover:scale-105 ">
+            <div className="w-[50px] h-[50px] rounded-full bg-[#004D3F] flex items-center justify-center text-white text-3xl font-semibold relative">
+              1
+            </div>
+
+            <h3 className="text-2xl font-medium text-gray-800 mt-6">
+              Onboarding
+            </h3>
+            <p className="text-gray-600 mt-3">
+              Analyze data to identify growth areas and provide actionable
+              solutions.
+            </p>
           </div>
 
-          <h3 className="text-2xl font-medium text-gray-800 mt-6">
-            Onboarding
-          </h3>
-          <p className="text-gray-600 mt-3">
-            Analyze data to identify growth areas and provide actionable
-            solutions.
-          </p>
-        </div>
-
-        <div className=" ml-22 w-full h-[320px] mt-  flex flex-col items-center justify-center p-6 text-center transition-transform transform hover:scale-105">
-          <div className="w-[50px] h-[50px]  rounded-full bg-[#004D3F] flex items-center justify-center text-white text-3xl font-semibold relative">
-            2
+          <div className=" w-full  flex flex-col items-center justify-center p-6 text-center transition-transform transform hover:scale-105 ">
+            <div className="w-[50px] h-[50px]  rounded-full bg-[#004D3F] flex items-center justify-center text-white text-3xl font-semibold relative">
+              2
+            </div>
+            <h3 className="text-2xl font-medium text-gray-800 mt-6">
+              Planning
+            </h3>
+            <p className="text-gray-600 mt-3">
+              Develop a comprehensive strategy based on insights and findings
+              from onboarding.
+            </p>
           </div>
-          <h3 className="text-2xl font-medium text-gray-800 mt-6">Planning</h3>
-          <p className="text-gray-600 mt-3">
-            Develop a comprehensive strategy based on insights and findings from
-            onboarding.
-          </p>
-        </div>
 
-        <div className="ml-44 w-full h-[320px] flex flex-col items-center justify-center p-6 text-center transition-transform transform hover:scale-105">
-          <div className="w-[50px] h-[50px] rounded-full bg-[#004D3F] flex items-center justify-center text-white text-3xl font-semibold relative">
-            3
+          <div className="w-full h-[320px] flex flex-col items-center justify-center p-6 text-center transition-transform transform hover:scale-105 ">
+            <div className="w-[50px] h-[50px] rounded-full bg-[#004D3F] flex items-center justify-center text-white text-3xl font-semibold relative">
+              3
+            </div>
+            <h3 className="text-2xl font-medium text-gray-800 mt-6">
+              Implementation
+            </h3>
+            <p className="text-gray-600 mt-3">
+              Execute the plan, monitor results, and optimize for sustained
+              success.
+            </p>
           </div>
-          <h3 className="text-2xl font-medium text-gray-800 mt-6">
-            Implementation
-          </h3>
-          <p className="text-gray-600 mt-3">
-            Execute the plan, monitor results, and optimize for sustained
-            success.
-          </p>
         </div>
-      </div>
+      </CommonWrapper>
       {/* Curved HR Line */}
 
-      <button className=" w-[234px] h-[60px] bg-[#A7EB94] text-green-900 font-semibold px-4 py-2 rounded-[5px] absolute left-[843px]">
+      <button className=" bg-[#A7EB94] text-green-900 font-semibold px-4 py-2 rounded-[5px] text-center flex mx-auto">
         Get In Teach
       </button>
     </div>
--- src/components/reUseAbles/ArticalCards.jsx
+++ src/components/reUseAbles/ArticalCards.jsx
@@ -1,39 +1,70 @@
-import React from 'react'
-import Button from './Buttons'
+import React from "react";
+import Button from "./Buttons";
 
-const ArticalCards = ({ layout = 'vertical', date, title, description, imgage }) => {
-    const isHorizontal = layout === 'horizontal';
+const ArticalCards = ({
+  layout = "vertical",
+  date,
+  title,
+  description,
+  imgage,
+}) => {
+  const isHorizontal = layout === "horizontal";
 
-    return (
+  return (
+    <div
+      className={`${
+        isHorizontal ? "w-full  p-[24px]" : "w-full lg:h-[800px] pt-[34px] "
+      } ${isHorizontal ? "flex-row" : "flex-col"} bg-white  rounded-3xl flex`}
+    >
+      <img
+        className={`${
+          isHorizontal ? "w-[279px] " : "w-[515px]"
+        } rounded-[21px] bg-gray-600 mx-auto object-cover`}
+        src={imgage}
+        alt="add img"
+      />
+
+      <div
+        className={`${
+          isHorizontal
+            ? "flex flex-col justify-between ml-[12px] w-full"
+            : "flex flex-col justify-between mt-[44px] w-[90%]"
+        } mx-auto`}
+      >
         <div
-            className={`${isHorizontal ? 'w-full h-[325px] p-[24px]' : 'w-full h-[687px] pt-[34px] '} ${isHorizontal ? 'flex-row' : 'flex-col'} bg-[#EDF3F2]  rounded-3xl flex`}
+          className={` flex justify-between items-center ${
+            isHorizontal ? "  mt-[22px]" : ""
+          }`}
         >
-            <img
-                className={`${isHorizontal ? 'w-[279px] h-[274px]' : 'w-[515px] h-[351px]'} rounded-[21px] bg-gray-600 mx-auto object-cover`}
-                src={imgage}    
-                alt="add img"
-            />
-
-            <div className={`${isHorizontal ? 'flex flex-col justify-between ml-[12px] w-full' : 'flex flex-col justify-between mt-[44px] w-[90%]'} mx-auto`}>
-
-
-                <div className={` flex justify-between items-center ${isHorizontal ? '  mt-[22px]' : ''}`}>
-                    <h6>{date}</h6>
-                    <Button border={false} w='w-[131px]' h='h-[49px]' c='bg-[#004D3F26]' />
-                </div>
-
-                <h4 className={`${isHorizontal ? ' ' : ''} font-raleway font-semibold text-[22px] leading-[36px] tracking-[0%] mt-[21px] `}>
-                    {title}
-                </h4>
-
-                <p className={`${isHorizontal ? '' : 'font-karla font-normal text-[16px] leading-[29.63px] tracking-[0%]  '} mt-[21px]`}>
-                   {description}
-                </p>
-
-
-            </div>
+          <h6>{date}</h6>
+          <Button
+            border={false}
+            w="w-[131px]"
+            h="h-[49px]"
+            c="bg-[#004D3F26]"
+          />
         </div>
-    )
-}
 
-export default ArticalCards
+        <h4
+          className={`${
+            isHorizontal ? " " : ""
+          } font-raleway font-semibold text-[22px] leading-[36px] tracking-[0%] mt-[21px] `}
+        >
+          {title}
+        </h4>
+
+        <p
+          className={`${
+            isHorizontal
+              ? ""
+              : "font-karla font-normal text-[16px] leading-[29.63px] tracking-[0%]  "
+          } mt-[21px]`}
+        >
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ArticalCards;
