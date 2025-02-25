3a517ec2aa75144c34a41813bd21d63d18fe5c1d
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-02-25 13:02:04 +0600

    articles, about page responsive fixed

--- src/components/reUseAbles/ArticalCards.jsx
+++ src/components/reUseAbles/ArticalCards.jsx
@@ -1,73 +1,60 @@
+// ArticalCards Component
 import React from "react";
 import Button from "./Buttons";
 
-
 const ArticalCards = ({
-
   layout = "vertical",
   date,
   title,
   description,
-
   imgage,
-
+  buttonText,
 }) => {
   const isHorizontal = layout === "horizontal";
 
   return (
     <div
-
-      className={`${
-        isHorizontal ? "w-full  p-[24px]" : "w-full lg:h-[800px] pt-[34px] "
-      } ${isHorizontal ? "flex-row" : "flex-col"} bg-white  rounded-3xl flex`}
+      className={`
+        ${
+          isHorizontal
+            ? "p-4 md:p-[18px]"
+            : "pt-[20px] md:pt-[34px] lg:py-[50px]"
+        }
+        ${isHorizontal ? "flex-row" : "flex-col"}
+        bg-white rounded-2xl lg:flex w-full md:w-auto shadow-md lg:py-[10px]
+      `}
     >
       <img
-        className={`${
-          isHorizontal ? "w-[279px] " : "w-[515px]"
-        } rounded-[21px] bg-gray-600 mx-auto object-cover`}
+        className={`
+          ${isHorizontal ? "w-[270px] md:w-[279px]" : "w-full md:w-[515px]"}
+          rounded-lg bg-gray-600 object-cover mx-auto
+        `}
         src={imgage}
         alt="add img"
       />
 
       <div
-        className={`${
-          isHorizontal
-            ? "flex flex-col justify-between ml-[12px] w-full"
-            : "flex flex-col justify-between mt-[44px] w-[90%]"
-        } mx-auto`}
+        className={`
+          ${
+            isHorizontal
+              ? "ml-4 md:ml-[20px] w-full"
+              : "mt-4 md:mt-[16px] w-full md:w-[90%]"
+          }
+          flex flex-col justify-between mx-auto
+        `}
       >
-        <div
-          className={` flex justify-between items-center ${
-            isHorizontal ? "  mt-[22px]" : ""
-          }`}
-        >
-          <h6>{date}</h6>
-
-          <Button
-            border={false}
-            w="w-[131px]"
-            h="h-[49px]"
-            c="bg-[#004D3F26]"
-          />
+        <div className="flex justify-between items-center mt-2 md:mt-[22px]">
+          <h6 className="text-sm md:text-base">{date}</h6>
+          <button className="p-3 bg-[#004D3F]/15 text-primary-button mx-4 rounded-md">
+            {buttonText}
+          </button>
         </div>
 
-
-        <h4
-          className={`${
-            isHorizontal ? " " : ""
-          } font-raleway font-semibold text-[22px] leading-[36px] tracking-[0%] mt-[21px] `}
-        >
+        <h4 className="font-raleway font-semibold text-lg md:text-[22px] leading-snug mt-2 md:mt-[21px]">
           {title}
         </h4>
 
-        <p
-          className={`${
-            isHorizontal
-              ? ""
-              : "font-karla font-normal text-[16px] leading-[29.63px] tracking-[0%]  "
-          } mt-[21px]`}
-        >
-
+        <p className="text-sm md:text-[16px] leading-relaxed mt-2 md:mt-[21px]">
           {description}
         </p>
       </div>
@@ -75,6 +62,4 @@ const ArticalCards = ({
   );
 };
 
-
 export default ArticalCards;
-
